7a3bbc51725395af633002606d7df40893b797e4
doublezed2 <doublezed2@gmail.com>
2021-11-26 14:18:26 +0500

    update db pass to zZ991231

--- db.php
+++ db.php
@@ -2,7 +2,7 @@
 date_default_timezone_set("Asia/Karachi");
 $servername = "localhost";
 $username = "root";
-$password = "";
+$password = "zZ991231";
 $dbname = "hms";
 $conn = new mysqli($servername, $username, $password, $dbname);
 if ($conn->connect_error) {

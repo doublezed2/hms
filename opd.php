a30e53c227cb6ca7e56b01e47cfbf3185b1a22f4
doublezed2 <doublezed2@gmail.com>
2021-12-01 19:18:53 +0500

    updated sort on view-appointments.php

--- opd.php
+++ opd.php
@@ -107,7 +107,7 @@ include("header.php");
                                 </div>
                                 <div class="form-group">
                                     <label>Mobile No.</label>
-                                    <input type="tel" name="p_phone" class="form-control" autocomplete="off" required tabindex="2" pattern="[0-9]{11}">
+                                    <input type="tel" name="p_phone" class="form-control" autocomplete="off" required tabindex="2">
                                 </div>
                                 <div class="form-group">
                                     <label>Choose Doctor</label>
